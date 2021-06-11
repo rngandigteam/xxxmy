649ac8ff71ff286a3e5be2eefce3f479673a1a7c
rngandigteam <jsstark@itcom888.com>
2021-06-11 23:35:59 +0800

    Update 添加备注

--- app/src/main/java/com/google/notetest/MainActivity.kt
+++ app/src/main/java/com/google/notetest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TEST测试
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
